b050a6a2054eed7fb839dbf20eee0a505fdef3db
zhanglei <cyourai@sina.com>
2019-05-04 17:05:23 +0800

    1.修bulldog

--- config/index.js
+++ config/index.js
@@ -13,8 +13,9 @@ module.exports = {
     proxyTable: {},
 
     // Various Dev Server settings
-    host: 'localhost', // can be overwritten by process.env.HOST
-    // host: '192.168.99.165',
+    // host: 'localhost', // can be overwritten by process.env.HOST
+
+    host: 'localhost',
     port: 9527, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: true,
     errorOverlay: true,
